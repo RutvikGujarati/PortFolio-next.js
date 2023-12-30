aa23b1a839170ff08c2ad6d3f5c9658ed1a5b389
Rutvik Gujarati <125241992+RutvikGujarati@users.noreply.github.com>
2023-12-31 00:22:46 +0530

    Update page.tsx

--- next-14/app/my-projects/page.tsx
+++ next-14/app/my-projects/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div
-    style={{backgroundImage: "url(/mountains.jpg)"}}
+    style={{backgroundImage: "url(/Mountains.jpg)"}}
      className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
         {Projects.map((project, index) => (
